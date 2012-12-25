1485f63b0cda55d65751bf01901c677f933e24fb
big <cleanedpath@gmail.com>
2012-12-25 20:09:36 +0100

    add entry plugin - logs all generated events

---
+++ life/plugs/entry.py
@@ -0,0 +1,24 @@
+# life/plugs/entry.py
+#
+#
+
+""" record entry of lived events. """
+
+## life imports
+
+from life import O
+from life.utils import strtotime
+
+## basic import
+
+import time
+
+## entry callbacks
+
+def entry(event):
+    print(event.json())
+    t = strtottime(event.input)
+    if t: event.enteredtime = t ; event.reply("time set at %s" % time.ctime(t))
+    event.save()
+
+entry.cb = "console"
